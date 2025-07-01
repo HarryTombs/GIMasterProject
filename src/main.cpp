8564a2786a3a854a5b26718cad899db1cf642ae8
HarryTombs <ht-23@live.co.uk>
2025-07-01 14:51:59 +0100

    wrong width and height

--- src/main.cpp
+++ src/main.cpp
@@ -169,7 +169,7 @@ void InitialiseProgram()
 
     if (data)
     {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ScreenWidth, ScreenHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else 
