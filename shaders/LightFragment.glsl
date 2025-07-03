b9f5692f5458ecc61e7a356daccab3bdd3911b63
HarryTombs <ht-23@live.co.uk>
2025-07-03 19:45:00 +0100

    Checking if gbuffer works

--- shaders/LightFragment.glsl
+++ shaders/LightFragment.glsl
@@ -1,7 +1,17 @@
 #version 430 core
 in vec2 uv;
 out vec4 color;
+
+uniform sampler2D gPos;
+uniform sampler2D gNorm;
+uniform sampler2D gAlbedoSpec;
+
+
+
 void main() 
     {
-        color = vec4(1.0,1.0,1.0,1.0);
+        vec4 fragPos = texture(gPos, uv);
+        vec3 Normal = texture(gNorm, uv).xyz;
+        vec3 Abledo = texture(gAlbedoSpec, uv).xyz;
+        color = vec4(fragPos.xyz,1.0);
     }
