db9d42fa0e095daecbe52dcbe35f54b1c0952318
HarryTombs <ht-23@live.co.uk>
2025-07-18 14:40:47 +0100

    Shaders added to pass system

--- include/Pass.h
+++ include/Pass.h
@@ -1,18 +1,46 @@
 #ifndef PASS_H
 #define PASS_H
 #include "FrameBufferObject.h"
+#include "ShaderUtils.h"
 #include <vector>
 #include <GL/glew.h>
+#include <string>
 
 
-class Pass 
+class Pass
 {
 public:
+
+    Pass(std::string vertpath, std::string fragpath, bool screenQuad = false)
+    {
+        vert = vertpath;
+        frag = fragpath;
+        isScreenQuad = screenQuad;
+    };
     std::vector<TextureObj> In;
     std::vector<TextureObj> Out;
     FrameBufferObject frameBuffer;
 
+    bool isScreenQuad;
+
+    unsigned int shaderProgram;
+
+    std::string vert;
+    std::string frag;
+    void init()
+    {
+        createShaderProgram();
+    }
     void execute();
+
+    void createShaderProgram()
+    {
+        unsigned int prog = loadShaderProgram(vert, frag);
+        shaderProgram = prog;
+    }
+    // Maybe consolidate into an init ()
+    // init shaders textures 
+
 
 
     // i'll probably need multiple computes
@@ -21,7 +49,16 @@ private:
 
     unsigned int vertex;
     unsigned int fragment;
+    unsigned int program;
     unsigned int compute;
+
+    
+
+
+    
+
+
+    bool checkTextures();
     void bindTextures()
     {
         int i = 0;
@@ -33,11 +70,15 @@ private:
         }
     }
 
+    
+    
+
 };
 
 struct Graph
 {
     std::vector<Pass> Passes;
+    float deltaTime;
 
     void executePasses()
     {
@@ -47,6 +88,40 @@ struct Graph
         }
     };
 };
+
+GLuint quadVAO,quadVBO;
+
+void createScreenQuad()
+{
+    
+    float screenQuadVerticies[] = 
+        {
+            1.0f,  1.0f, 0.0f,   1.0f, 1.0f,
+            1.0f, -1.0f, 0.0f,   1.0f, 0.0f,
+            -1.0f,  1.0f, 0.0f,   0.0f, 1.0f,
+            1.0f, -1.0f, 0.0f,   1.0f, 0.0f,
+            -1.0f, -1.0f, 0.0f,   0.0f, 0.0f,
+            -1.0f,  1.0f, 0.0f,   0.0f, 1.0f
+        };
+
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(screenQuadVerticies),&screenQuadVerticies,GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3,GL_FLOAT, GL_FALSE, 5 * sizeof(float),(void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2,GL_FLOAT, GL_FALSE, 5 * sizeof(float),(void*)(3*sizeof(float)));
+
+}
+
+void renderQuad()
+{
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+}
 
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/string_cast.hpp>
 
-// #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 #include <filesystem>
 
@@ -19,6 +19,7 @@
 #include "ShaderUtils.h"
 #include "Mesh.h"
 #include "Camera.h"
+#include "Pass.h"
 #include "FrameBufferObject.h"
 
 int ScreenHeight = 512;
@@ -27,22 +28,20 @@ SDL_Window* GraphicsApplicationWindow = nullptr;
 SDL_GLContext OpenGlConext = nullptr;
 bool gQuit = false;
 
-bool upPress = false;
-bool downPress = false;
-bool rightPress = false;
-bool leftPress = false;
-
 bool mouseDown = false;
 float glX;
 float glY;
 Model* cubeModel;
 Camera fpsCamera(glm::vec3(0.0f,0.0f,3.0f));
 
-GLuint renderShader, gbufferShader, lightShader; 
-GLuint quadVAO,quadVBO;
+GLuint renderShader; 
+
 
 FrameBufferObject gBufferFBO;
 TextureObj GPos, GNorm, GAlbSpec;
+
+Pass lightspass("shaders/LightVertex.glsl","shaders/LightFragment.glsl");
+Pass GbufferPass("shaders/DSVertex.glsl", "shaders/DSFragment.glsl");
 
 TextureFormat GPosFmt = {GL_RGB16F, GL_RGBA, GL_FLOAT };
 TextureFormat GNormFmt = {GL_RGB16F, GL_RGBA, GL_FLOAT };
@@ -59,15 +58,7 @@ double deltaTime = 0;
 unsigned int texture;
 
 
-float screenQuadVerticies[] = 
-{
-    1.0f,  1.0f, 0.0f,   1.0f, 1.0f,
-    1.0f, -1.0f, 0.0f,   1.0f, 0.0f,
-   -1.0f,  1.0f, 0.0f,   0.0f, 1.0f,
-    1.0f, -1.0f, 0.0f,   1.0f, 0.0f,
-   -1.0f, -1.0f, 0.0f,   0.0f, 0.0f,
-   -1.0f,  1.0f, 0.0f,   0.0f, 1.0f
-};
+
 
 
 
@@ -243,8 +234,9 @@ void InitialiseProgram()
     cubeModel = new Model(modelPath);
 
     renderShader = loadShaderProgram("shaders/vertex.glsl", "shaders/fragment.glsl");
-    gbufferShader = loadShaderProgram("shaders/DSVertex.glsl", "shaders/DSFragment.glsl");
-    lightShader = loadShaderProgram("shaders/LightVertex.glsl", "shaders/LightFragment.glsl");
+
+    lightspass.createShaderProgram();
+    GbufferPass.createShaderProgram();
 
     if (renderShader == 0) {
         std::cerr << "Failed to load shaders." << std::endl;
@@ -279,15 +271,7 @@ void InitialiseProgram()
 
     // Create Screen Quad
 
-    glGenVertexArrays(1, &quadVAO);
-    glGenBuffers(1, &quadVBO);
-    glBindVertexArray(quadVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(screenQuadVerticies),&screenQuadVerticies,GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3,GL_FLOAT, GL_FALSE, 5 * sizeof(float),(void*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2,GL_FLOAT, GL_FALSE, 5 * sizeof(float),(void*)(3*sizeof(float)));
+    createScreenQuad();
 
     /// GBuffer Creation
 
@@ -311,7 +295,7 @@ void InitialiseProgram()
     glDrawBuffers(3, attachments);
 
     unsigned int rboDepth;
-    glGenRenderbuffers(1, &rboDepth);
+    glGenRenderbuffers(1, &rboDepth);   
     glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
     glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, ScreenWidth , ScreenHeight);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
@@ -323,10 +307,10 @@ void InitialiseProgram()
         std::cout << "Gbuffer Complete" << std::endl;
     }
 
-    glUseProgram(lightShader);
-    glUniform1i(glGetUniformLocation(lightShader, "gPosition"), 0);
-    glUniform1i(glGetUniformLocation(lightShader, "gNorm"), 1);
-    glUniform1i(glGetUniformLocation(lightShader, "gAlbedoSpec"), 2);
+    glUseProgram(lightspass.shaderProgram);
+    glUniform1i(glGetUniformLocation(lightspass.shaderProgram, "gPosition"), 0);
+    glUniform1i(glGetUniformLocation(lightspass.shaderProgram, "gNorm"), 1);
+    glUniform1i(glGetUniformLocation(lightspass.shaderProgram, "gAlbedoSpec"), 2);
 
     // lights
 
@@ -457,7 +441,7 @@ void MainLoop() {
 
         gBufferFBO.bind();
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        LoadMatricies(gbufferShader,true);
+        LoadMatricies(GbufferPass.shaderProgram,true);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
         cubeModel->Draw();
@@ -469,7 +453,7 @@ void MainLoop() {
         // Light Pass
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glUseProgram(lightShader);
+        glUseProgram(lightspass.shaderProgram);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, GPos.texID);
         glActiveTexture(GL_TEXTURE1);
@@ -481,23 +465,21 @@ void MainLoop() {
 
         for (unsigned int i = 0; i < lightPos.size(); i++)
         {
-            setVec3(lightShader,("lights[" + std::to_string(i) + "].Position"), lightPos[i]);
-            setVec3(lightShader,("lights[" + std::to_string(i) + "].Color"), lightCol[i]);
+            setVec3(lightspass.shaderProgram,("lights[" + std::to_string(i) + "].Position"), lightPos[i]);
+            setVec3(lightspass.shaderProgram,("lights[" + std::to_string(i) + "].Color"), lightCol[i]);
             const float linear = 0.7f;
             const float quadratic = 1.8f;
-            setFloat(lightShader,("lights[" + std::to_string(i) + "].Linear"), linear);
-            setFloat(lightShader,("lights[" + std::to_string(i) + "].Quadratic"), quadratic);
+            setFloat(lightspass.shaderProgram,("lights[" + std::to_string(i) + "].Linear"), linear);
+            setFloat(lightspass.shaderProgram,("lights[" + std::to_string(i) + "].Quadratic"), quadratic);
         }
-        setVec3(lightShader,"viewPos", fpsCamera.CamPos);
+        setVec3(lightspass.shaderProgram,"viewPos", fpsCamera.CamPos);
         CheckGLError("Light creation");
 
         glDisable(GL_DEPTH_TEST);
 
         // Render screen quad
 
-        glBindVertexArray(quadVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        glBindVertexArray(0);
+        renderQuad();
 
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
